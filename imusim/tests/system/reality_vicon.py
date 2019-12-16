e56f4ad1cab80f765356411397b7c5bd97126bf0
CantCode858 <arshpetrosyan@gmail.com>
2019-12-16 14:08:40 -0500

    modified:   reality_vicon.py

--- imusim/tests/system/reality_vicon.py
+++ imusim/tests/system/reality_vicon.py
@@ -31,26 +31,39 @@ def vicon_test():
     vicon_object = SplinedMarkerCapture(vcsv.loadViconCSVFile(vicon_file))
     jointNames = vcsv.get_joints(vicon_file)
     if "L" in vicon_file:
-        imu_marker = "L_RS"
-        print("IMU marker set to left hand.")
+        imu_marker = vicon_object.marker("L_RS")
+        print("IMU markers set to left hand.")
+        jointMarkerNames = ["L_ME", "L_RS", "L_MCP2"]
+        refMarkerNames = ["L_ME", "L_MCP2"]
+        print("Reference markers set.")
     elif "R" in vicon_file:
-        imu_marker = "R_RS"
+        imu_marker = vicon_object.marker("R_RS")
         print("IMU marker set to right hand.")
+        jointMarkerNames = ["R_ME", "R_RS", "R_MCP2"]
+        refMarkerNames = ["R_ME", "R_MCP2"]
+        print("Reference markers set.")
     else:
         print("IMU marker can not be set.")
         return
-    # jointMarkerSets = lambda c: [
-    #     list(map(c.marker, jointNames))
-    # ]
-    imuMarkerSets = lambda c: [
-        [c.marker(imu_marker)]
+    jointMarkerSets = lambda c: [
+        list(map(c.marker, jointMarkerNames)),
+        [list(map(c.marker, refMarkerNames))]
+        # c.marker(imu_marker)
     ]
-    print(vicon_object)
-    # jointTrajectories = [MultiMarkerTrajectory(j, r+i, refTime=refTime)
-    #     for j, r, i in zip(*(jointMarkerSets(vicon_object)))
+    # imuMarkerSets = lambda c: [
+    #     [c.marker(imu_marker)]
     # ]
-    imuVecTrajectories = [MultiMarkerTrajectory(p, refTime=refTime)
-        for p in imuMarkerSets(vicon_object)]
+    # print(vicon_object)
+    jointTrajectories = [MultiMarkerTrajectory(j, r+imu_marker, refTime=refTime)
+        for j, r in zip(*(jointMarkerSets(vicon_object)))
+    ]
+    # imuVecTrajectories = [MultiMarkerTrajectory(p, refTime=refTime)
+    #     for p in imuMarkerSets(vicon_object)]
+    print(jointTrajectories)
     print("We good.")
+    # markers = [map(vicon_object.marker, jointNames)]
+    # jointTrajectories = [MultiMarkerTrajectory(j, r+i, refTime=refTime)
+    #     for j, r, i in markers]
+    # print("We good.")
 
 vicon_test()
