38633c6dcf98b2caa6d5c8da5b9ba4e123e2cd14
CantCode858 <arshpetrosyan@gmail.com>
2019-12-18 17:01:10 -0500

    modified:   reality_vicon.py

--- imusim/tests/system/reality_vicon.py
+++ imusim/tests/system/reality_vicon.py
@@ -22,38 +22,49 @@ def vicon_test():
     # Define dir (empty char instead of __file__, works in jupyter nb).
     dir = path.dirname('')
     # Define some constants.
-    refTime = 1.0
+    refTime = 5.0
     # posStdDev = 0.0005
     # rotStdDev = 0.004
     # Load the files.
+    ref_file = "S01_cal.csv"
     vicon_file = "S01_E2_L_1.csv"
+    cal = vcsv.loadViconCSVFile(ref_file)
     csv = vcsv.loadViconCSVFile(vicon_file)
     # smartwatch_file = vicon_file + ".gz"
+    ref_object = SplinedMarkerCapture(cal)
     vicon_object = SplinedMarkerCapture(csv)
-    jointNames = vcsv.get_joints(vicon_file)
     if "L" in vicon_file:
         imu_hand = "L_"
         print("IMU markers set to left hand.")
-        jointMarkerNames = ["L_ME", "L_RS", "L_MCP2"]
-        print("Reference markers set.")
     elif "R" in vicon_file:
         imu_hand = "R_"
         print("IMU marker set to right hand.")
-        jointMarkerNames = ["R_ME", "R_RS", "R_MCP2"]
-        print("Reference markers set.")
     else:
         print("IMU marker can not be set.")
         return
-    imu_marker = vicon_object.marker(imu_hand + "RS")
-    refMarkerNames = [vicon_object.marker(ref) \
-        for ref in [imu_hand + "ME", imu_hand + "MCP2"]]
-    jointMarkerNames = [vicon_object.marker(joint) \
-        for joint in [imu_hand + "ME", imu_hand + "RS", imu_hand + "MCP2"]]
+    imu_marker = imu_hand + "RS"
+    # imu_marker = vicon_object.marker(imu_hand + "RS")
+    refMarkerNames = [imu_hand + "ME", imu_hand + "MCP2"]
+    # refMarkerNames = [vicon_object.marker(ref) \
+    #     for ref in [imu_hand + "ME", imu_hand + "MCP2"]]
+    jointMarkerNames = [imu_hand + "ME", imu_hand + "RS", imu_hand + "MCP2"]
+    # jointMarkerNames = [vicon_object.marker(joint) \
+    #     for joint in [imu_hand + "ME", imu_hand + "RS", imu_hand + "MCP2"]]
+    jointMarkerSets = lambda c: [
+        list(map(c.marker, jointMarkerNames)),
+        list(map(c.marker, refMarkerNames)),
+        c.marker(imu_marker)
+    ]
+    jointRefTrajectories = [MultiMarkerTrajectory(j, r + i, refTime=refTime) \
+        for j, r, i in zip(*(jointMarkerSets(ref_object)))]
     imuTrajectory = \
         MultiMarkerTrajectory(imu_marker, refMarkerNames, refTime=refTime)
-    print(imuTrajectory)
+    # print(imuTrajectory)
+    print(jointRefTrajectories)
     print("We good.")
     joints = []
     for i in range(len(jointMarkerNames)):
         name = jointMarkerNames[i]
     return imuTrajectory
+
+vicon_test()
